0f0a0d1c4dd259cd63d681a63fe37d3b59064c65
Alexander Savoulides <alexandersavoulides@gmail.com>
2023-01-06 22:44:25 -0500

    Changed to Returns

--- API.py
+++ API.py
@@ -22,27 +22,39 @@ data2 = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_DA
 close_prices1 = [float(data1[date]["4. close"]) for date in data1]
 close_prices2 = [float(data2[date]["4. close"]) for date in data2]
 
-# Calculate the Pearson correlation coefficient and the p-value
-r, p = pearsonr(close_prices1, close_prices2)
+# Calculate the returns for each stock
+returns1 = []
+for i in range(1, len(close_prices1)):
+  return1 = (close_prices1[i] - close_prices1[i - 1]) / close_prices1[i - 1]
+  returns1.append(return1)
 
-# Convert the close prices to NumPy arrays
-X = np.array(close_prices1).reshape(-1, 1)
-y = np.array(close_prices2)
+returns2 = []
+for i in range(1, len(close_prices2)):
+  return2 = (close_prices2[i] - close_prices2[i - 1]) / close_prices2[i - 1]
+  returns2.append(return2)
+
+# Calculate the Pearson correlation coefficient and the p-value
+r, p = pearsonr(returns1, returns2)
+
+# Convert the returns to NumPy arrays
+X = np.array(returns1).reshape(-1, 1)
+y = np.array(returns2)
 
 # Fit a linear regression model
 model = LinearRegression()
 model.fit(X, y)
 
-# Get the R^2 value
+# Get the R^2 value and the slope of the line
 r_squared = model.score(X, y)
+slope = model.coef_[0]
 
-# Predict the close prices of the second stock based on the close prices of the first stock
+# Predict the returns of the second stock based on the returns of the first stock
 y_pred = model.predict(X)
 
 # Plot the data and the line of best fit
-plt.scatter(close_prices1, close_prices2)
-plt.plot(close_prices1, y_pred, color='red')
-plt.title(f"{symbol1} vs. {symbol2} (R^2 = {r_squared:.2f})")
+plt.scatter(returns1, returns2)
+plt.plot(returns1, y_pred, color='red')
+plt.title(f"{symbol1} vs. {symbol2} (R^2 = {r_squared:.2f}, slope = {slope:.2f})")
 plt.xlabel(symbol1)
 plt.ylabel(symbol2)
 #plt.legend([symbol1, symbol2])
